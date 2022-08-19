6078de5335da10de3cd8935ba2bbce830ee75b96
Mauro Delmondes <maurodelmondes@hotmail.com>
2022-08-19 15:55:50 -0400

    Criando funções para capturar os dados inseridos e adiciona-los na lista

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nome = e.target.elements['nome'].value;
+    const quantidade = e.target.elements['quantidade'].value;
+
+    criaElemento(nome, quantidade);
+});
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+}
